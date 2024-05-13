aea6065ac9be04cf3667cb9ea732dd099bd3e762
LyraCatriel <151699525+LyraCatriel@users.noreply.github.com>
2024-05-13 18:00:24 +0000

    Simulates single round

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-console.log("Hello World");
+console.log("Let's play Rock/Paper/Scissors!");
 
 /* 
-Gets a random number between 0-2 (inclusive), depending on the number, returns one of the following string values: "rock", "paper", or "scissors"
+valid moves are rock, paper, or scissors
+number is random number between 0 and 2 (inclusive)
+computer's move is number index of valid moves
 */
-function getComputerChoice()
-{
+function getComputerChoice() {
     const validMoves = ["rock", "paper", "scissors"];
     const minCeiled = Math.ceil(0);
     const maxFloored = Math.floor(3);
-    let compMove = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-    return validMoves[compMove];
+    let num = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    console.log("computer chose " + validMoves[num]);
+    return num;
 }
 
 /*
 Get user input ("Do you want to play rock, paper, or scissors?"), return input
 */
-function getHumanChoice()
-{
+function getHumanChoice() {
+    const validMoves = ["rock", "paper", "scissors"];
     let humanChoice = prompt("Do you want to play rock, paper, or scissors?");
     // if (humanChoice.toLowerCase() === "rock" || "paper" || "scissors")
-    return humanChoice;
+    console.log("you chose " + humanChoice.toLowerCase());
+    num = validMoves.indexOf(humanChoice.toLowerCase());
+    
+    return num; 
 }
 
+/*
+rock is 0
+paper is 1
+scissors is 2
+if (human - computer) mod 3 is 1, human wins
+2 computer wins
+otherwise tie
+*/
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let winner = "No Winner"
+    let result = (mod((humanChoice - computerChoice), 3));
+    if (result === 1) {
+        winner = "Human";
+    }
+    else if (result === 2) {
+        winner = "Computer";
+    }
+    else {
+        winner = "Tie";
+    }
+    return winner;
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()) + " wins");
